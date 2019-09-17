f8c99f78a2cccbeb161fc1d7f142667c75ba4452
queenss <54778677+queenss@users.noreply.github.com>
2019-09-18 03:48:27 +0530

    Update balaji.py

--- stdplugins/balaji.py
+++ stdplugins/balaji.py
@@ -28,9 +28,9 @@ async def _(event):
 
         animation_chars = [
         
-            "`Your bot is running\n\nTelethon version:` 1.9.0\n`Python:` 3.7.3\n`User:` @r4v4n4\n`Database Status: Databases functioning normally!`",
+            "`Your bot is running\n\nTelethon version:` 1.9.0\n`Python:` 3.7.3\n`User:` @uniborg\n`Database Status: Databases functioning normally!`",
             "`Connecting To github.com...`",
-            "`Deleting Baalaji Repo....`",
+            "`Deleting spechide Repo....`",
             "`Forking Uniborg... 0%\n\n⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️\n\nFile Size: 0 MiB / 108.7 MiB`",
             "`Forking Uniborg... 4%\n\n⬛️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️\n\nFile Size: 22 MiB / 108.7 MiB`",
             "`Forking Uniborg... 8%\n\n⬛️⬛️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️\n\nFile Size: 48 MiB / 108.7 MiB`",    
@@ -39,8 +39,8 @@ async def _(event):
             "`Forking Uniborg... 52%\n\n⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬜️⬜️⬜️⬜️⬜️\n\nFile Size: 90.7 MiB / 108.7 MiB `",
             "`Forking Uniborg... 84%\n\n⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬜️⬜️\n\nFile Size: 100.7 MiB / 108.7 MiB `",
             "`Forking Uniborg... 100%\n\n⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️⬛️\n\nFile Size: 108.7 MiB / 108.7 MiB\n\nTask Completed... `",
-            "`Fork Deploying...`\n\n@UniBorg ( `Custom Built By` @kirito6969 ) \n`Verified Account:` ☑️\n`Official Website:` https://nehi hain koi website bhag bsdk\n\n`Python` `Loading...`\n[GCC 7.3.0]\n`Telethon` `Loading...`\n\n`Custom Built Fork:` `Loading...`",
-            "`Fork Deployed...`\n\n@UniBorg ( `Custom Built By` @kirito6969 ) \n`Verified Account:` ✅\n`Official Website:` https://nehi hain koi website bhag bsdk\n\n`Python` 3.6.8 (default, Jan 29 2019, 19:35:16)\n[GCC 7.3.0]\n`Telethon` 1.8.0\n\n`Custom Built Fork:` https://github.com/prono69/PornHub"
+            "`Fork Deploying...`\n\n@UniBorg ( `Custom Built By` @dieaena ) \n`Verified Account:` ☑️\n`Official Website:` https://@selectivegramophone bsdk\n\n`Python` `Loading...`\n[GCC 7.3.0]\n`Telethon` `Loading...`\n\n`Custom Built Fork:` `Loading...`",
+            "`Fork Deployed...`\n\n@UniBorg ( `Custom Built By` @dieaena ) \n`Verified Account:` ✅\n`Official Website:` https://@trendingjobe bsdk\n\n`Python` 3.6.8 (default, Jan 29 2019, 19:35:16)\n[GCC 7.3.0]\n`Telethon` 1.8.0\n\n`Custom Built Fork:` https://github.com/dieana/userbot"
         ]
 
         for i in animation_ttl:
