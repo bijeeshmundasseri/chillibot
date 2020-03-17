19ef837c6a1f0d9119ee5116b9cb0b3408dbf7c0
bijeeshmundasseri <62299810+bijeeshmundasseri@users.noreply.github.com>
2020-03-17 22:21:08 +0530

    Update auto_profile.py

--- stdplugins/auto_profile.py
+++ stdplugins/auto_profile.py
@@ -45,7 +45,7 @@ async def _(event):
     while True:
         DM = time.strftime("%d.%m.%y")
         HM = time.strftime("%H:%M")
-        name = f"⌚{HM}|🚶‍♂️AnonymousD3061🚶‍♂️|📅{DM}"
+        name = f"⌚{HM}|🚶‍♂️malavikamaluo🚶‍♂️|📅{DM}"
         logger.info(name)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
